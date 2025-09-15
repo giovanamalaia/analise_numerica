3cc395eec7b440f30fed4a475349e5f11144b9f3
Giovana <71229661+giovanamalaia@users.noreply.github.com>
2025-09-15 18:23:06 -0300

    Add files via upload

---
+++ raiz.h
@@ -0,0 +1,13 @@
+//
+//  raiz.h
+//  lab2_analise
+//
+//  Created by Giovana Malaia on 04/09/25.
+//
+
+#ifndef RAIZ_BISSECAO_H
+#define RAIZ_BISSECAO_H
+
+int bissecao (double a, double b, double (*f) (double x), double* r);
+
+#endif
